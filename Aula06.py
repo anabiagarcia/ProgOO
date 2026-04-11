b628f6931f8ef1999f33bc8347715ca35d5aa449
anabiagarcia <anabia.garcia29@gmail.com>
2026-04-10 21:04:30 -0300

    feat: aula 07

--- Aula06.py
+++ Aula06.py
@@ -7,52 +7,88 @@ class Notification(ABC):
         ...
 
 class SmsNotification(Notification):
-    def send(self, message, reciver) -> None:
+    def send(self, message, reciver):
         print(f"SMS: {message} To: {reciver}")
 
 class EmailNotification(Notification):
-    def send(self, message, reciver) -> None:
+    def send(self, message, reciver):
         print(f"Email: {message} To: {reciver}")
 
 class PushNotification(Notification):
-    def send(self, message, reciver) -> None:
+    def send(self, message, reciver):
         print(f"Push: {message} To: {reciver}")
+
+class SmsAdapter(Notification):
+    def __init__(self):
+        self.servico = SmsNotification()
+
+    def send(self, message, reciver):
+        self.servico.disparar_sms(reciver, message)
 
 class NotificationFactory:
     _types = {
-            "sms": SmsNotification,
-            "email": EmailNotification,
-            "push": PushNotification,
-        }
+        "sms": SmsNotification,
+        "email": EmailNotification,
+        "push": PushNotification,
+    }
+
     @staticmethod
     def _create(notification_type: str) -> Notification:
         new = NotificationFactory._types.get(notification_type)
         return new
-    
+
+
 class CoreComponent():
     _instance = None
 
-    def _new_(cls):
+    def __new__(cls):
         if cls._instance is None:
-            cls.instance = super().new_(cls)
+            cls._instance = super().__new__(cls)
             cls._instance._initialized = False
         return cls._instance
 
     def CreateInstance(self):
         if not self._initialized:
-            self.EmailNotification = NotificationFactory._create("email")()
-            self.SmsNotification = NotificationFactory._create("sms")()
-            self.PushNotification = NotificationFactory._create("push")()
+            self.Email = NotificationFactory._create("email")()
+            self.Sms = NotificationFactory._create("sms")()
+            self.Push = NotificationFactory._create("push")()
             self._initialized = True
 
     def getInstance(self, notification_type: str):
-        self.CreateInstance()
+        if not hasattr(self, "Email"):
+            self.CreateInstance()
         if notification_type == "email":
-            return self.EmailNotification
+            return self.Email
         elif notification_type == "sms":
-            return self.SmsNotification
+            return self.Sms
         elif notification_type == "push":
-            return self.PushNotification
-       
+            return self.Push
+
+    def send(self, notification_type: str, message: str, reciver: str):
+        self.getInstance(notification_type).send(message, reciver)
+
+class NotificationProxy:
+    def __init__(self, adapter: CoreComponent):
+        self.adapter = adapter
+
+    def send(self, notification_type: str, message: str, reciver: str):
+        print("Entrando no proxy")
+
+        if notification_type == "email" and "@" not in reciver:
+            print(f"Proxy bloqueou: '{reciver}' não é email válido")
+        else:
+            self.adapter.send(notification_type, message, reciver)
+
+        print("Saindo do proxy")
 
 
+class SmsNotification:
+    def disparar_sms(self, numero: str, texto: str):
+        print(f"SMS: {texto} To: {numero}")
+
+class SmsAdapter(Notification):
+    def __init__(self):
+        self.servico = SmsNotification()
+
+    def send(self, message, reciver):
+        self.servico.disparar_sms(reciver, message)
